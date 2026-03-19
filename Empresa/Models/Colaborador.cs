5d4416e144dc344c1eed1c1180e5421ea75c02b7
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2026-03-19 14:32:24 +0000

    feat: code done

--- Empresa/Models/Colaborador.cs
+++ Empresa/Models/Colaborador.cs
@@ -22,3 +22,15 @@ namespace Empresa.Models
     }
 }
 
+// Essa classe ta garantindo os 3 pilares da POO, deixamos ela abstract pelo fato dela estar imcompleta
+// e nao podermos instanciar ela diretamente. Não podemos criar um novo colaborador, essa classe serve apenas
+// para ser herdada. O colaborador necessita ser ou um efetivo ou um freelancer nesse caso.
+//O abstract aqui serve para dizer que ela esta imcompleta e precisa de atributos das filhas.
+//Ao usar o abstract no metodo de calcular o vencimento, estamos dizendo que todas suas filhas 
+// vão herdar esse metodo e que é obrigatorio ter o seu proprio metodo de calcular. Êstamos obrigado
+// as suas filhas a terem q calcular com esse metodo, proprio para cada filha.
+// por isso usamos o override nas filhas e definimos o metodo proprio em cada filha
+// O Protected é o ENCAPSULAMENTO, usei no salario base pq quero que as filhas consigam usam esse valor nos seus metodos e dados, mas nao quero
+// que consigam alterar esse valor usando alguma coisa do tipo colab.SalarioBase = 0
+// Ja o metodo publico do GetSalarioBase, foi criado para que eu conseguisse mostrar os valor do SalarioBase nas labels, por ele estar protected, so consigo
+// mostrar assim. Tipo uma janela de leitura, consigo mostrar, mas nao alterar
