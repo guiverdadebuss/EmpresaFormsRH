38922d3a6bd974ccbc98bf6c38cb98537c5fb98e
guiverdadebuss <guilhermeverdadebuss@gmail.com>
2026-03-20 00:36:08 +0000

    feat: fixing small things

--- Empresa/FormMenu.cs
+++ Empresa/FormMenu.cs
@@ -10,22 +10,25 @@ namespace Empresa
         private void btnAbrirRegisto_Click(object sender, EventArgs e)
         {
             FormRegisto reg = new FormRegisto();
+            this.Hide();
             reg.ShowDialog();
-
+            this.Show();
         }
 
         private void btnAbrirDetalhes_Click(object sender, EventArgs e)
         {
             FormDetalhes det = new FormDetalhes();
+            this.Hide();
             det.ShowDialog();
-
+            this.Show();
         }
 
         private void btnAbrirEstatisticas_Click(object sender, EventArgs e)
         {
             FormEstatisticas est = new FormEstatisticas();
+            this.Hide();
             est.ShowDialog();
-
+            this.Show();
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
